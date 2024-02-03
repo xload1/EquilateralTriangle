2b9ad583d34c8dd08ed276c2809512ca4eb1262e
xload1 <maratichts@gmail.com>
2024-02-03 04:22:19 +0100

    =)

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int height = new Scanner(System.in).nextInt();
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= consoleYtoX(height - i) / 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= consoleYtoX(i); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static double consoleYtoX(double h) {
+        return h * 2.6 * Math.sqrt(2);
+    }
+}
