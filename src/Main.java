200e18ca94fe3a4b90448237426a37c0d1c03a0c
xload1 <maratichts@gmail.com>
2024-02-03 04:23:17 +0100

    =)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int height = new Scanner(System.in).nextInt();
+
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= consoleYtoX(height - i) / 2; j++) {
                 System.out.print(" ");
